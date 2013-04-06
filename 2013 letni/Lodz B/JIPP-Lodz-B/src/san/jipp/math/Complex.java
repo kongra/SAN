02bf7cc09757eec4e9db5c9a5de4eed153d33358
Konrad Grzanek <kongra@gmail.com>
2013-04-06 09:19:44 +0200

    Generacja metod equals i hashCode dla klasy Complex.

--- 2013 letni/Lodz B/JIPP-Lodz-B/src/san/jipp/math/Complex.java
+++ 2013 letni/Lodz B/JIPP-Lodz-B/src/san/jipp/math/Complex.java
@@ -15,6 +15,29 @@ public class Complex {
     this(other, 0);
   }
 
+  public double modulus() {
+    return Math.sqrt(this.re * this.re + this.im * this.im);
+  }
+
+  public double argument() {
+    if (this.re > 0) {
+      return Math.atan(this.im / this.re);
+    }
+    else if (this.re < 0 && this.im >= 0) {
+      return Math.atan(this.im / this.re) + Math.PI;
+    }
+    else if (this.re < 0 && this.im < 0) {
+      return Math.atan(this.im / this.re) - Math.PI;
+    }
+    else if (this.re == 0 && this.im > 0) {
+      return Math.PI / 2;
+    }
+    else if (this.re == 0 && this.im < 0) {
+      return -Math.PI / 2;
+    }
+    return Double.NaN;
+  }
+
   public Complex add(Complex other) {
     return new Complex(this.re + other.re, this.im + other.im);
   }
@@ -39,4 +62,46 @@ public class Complex {
     buf.append(")");
     System.out.println(buf.toString());
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    long temp;
+    temp = Double.doubleToLongBits(im);
+    result = prime * result + (int) (temp ^ (temp >>> 32));
+    temp = Double.doubleToLongBits(re);
+    result = prime * result + (int) (temp ^ (temp >>> 32));
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    Complex other = (Complex) obj;
+    if (Double.doubleToLongBits(im) != Double.doubleToLongBits(other.im)) {
+      return false;
+    }
+    if (Double.doubleToLongBits(re) != Double.doubleToLongBits(other.re)) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder();
+    builder.append("Complex [im=").append(im).append(", re=").append(re)
+        .append("]");
+    return builder.toString();
+  }
+
 }
--- 2013 letni/Lodz B/JIPP-Lodz-B/src/san/jipp/math/TestComplex.java
+++ 2013 letni/Lodz B/JIPP-Lodz-B/src/san/jipp/math/TestComplex.java
@@ -10,12 +10,28 @@ public class TestComplex {
 
     Complex c = a.add(b);
 
-    a.print();
-    b.print();
-    c.print();
+//    a.print();
+//    b.print();
+//    c.print();
 
     Complex d = a.add(4);
-    d.print();
+//    d.print();
+
+//    System.out.println(d.modulus());
+//    System.out.println(d.argument());
+//
+//    Complex e = new Complex(0, 0);
+//    System.out.println(e.modulus());
+//    System.out.println(e.argument());
+
+    Complex f = new Complex(1, 2);
+    Complex g = new Complex(1, 2);
+
+    System.out.println(f.equals(g));
+    System.out.println(f.hashCode());
+    System.out.println(g.hashCode());
+    
+    System.out.println(f);
   }
 
 }
