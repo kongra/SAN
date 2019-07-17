29f736743aaa2e78d1b68bf9b186636212e88f00
kongra <kongra@gmail.com>
2019-07-17 09:25:25 +0200

    Update.

--- 2019 letni (Spring)/Algorytmy i złożoność - stacjonarne/AlgoLodz/src/san/lodz/algo/utils/TestLazySeq.java
+++ 2019 letni (Spring)/Algorytmy i złożoność - stacjonarne/AlgoLodz/src/san/lodz/algo/utils/TestLazySeq.java
@@ -6,17 +6,18 @@ import static san.lodz.algo.utils.LazySeq.take;
 public class TestLazySeq {
 
   public static void main(String... args) {
-    // Seq<Integer> N = iterate(n -> n + 1, 0);
-    // System.out.println(take(100, N).asString());
+    Seq<Integer> N = iterate(n -> n + 1, 0);
 
-//    System.out.println(N.first());
-//    System.out.println(N.rest().first());
-//    System.out.println(N.rest().rest().first());
-//    System.out.println(N.rest().rest().rest().first());
+    System.out.println(N.first());
+    System.out.println(N.rest().first());
+    System.out.println(N.rest().rest().first());
+    System.out.println(N.rest().rest().rest().first());
 
-    Seq<String> s1 = LinkedSeq.make("a", "b", "c", "a", "d", "b", "a");
-    Seq<String> s2 = s1.filter(s -> s.equals("a") || s.equals("b"));
-    System.out.println(s2.asString());
+    System.out.println(take(100, N).asString());
+
+//    Seq<String> s1 = LinkedSeq.make("a", "b", "c", "a", "d", "b", "a");
+//    Seq<String> s2 = s1.filter(s -> s.equals("a") || s.equals("b"));
+//    System.out.println(s2.asString());
   }
 
 }
