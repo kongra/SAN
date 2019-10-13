dc10dfcee4a10ed6d4ea90419eabdd8578098551
kongra <kongra@gmail.com>
2019-10-13 12:43:42 +0200

    Update.

---
+++ 2019 zimowy/PP dzienne/2019-10-09 (W)/Program.java
@@ -0,0 +1,13 @@
+// TYP DANYCH
+// w-g D. Scotta i Ch. Strachey'a
+//
+// 1. Jest to zbiór więzów i ograniczeń nakładanych
+//    na dane (obiekty) w celu zagwarantowania ich
+//    poprawnego użycia w wyrażeniach języka.
+//
+// 2. Jest to zbiór danych (obiektów) spełniających
+//    ten sam zestaw więzów i ograniczeń.
+
+class Program {
+
+}
