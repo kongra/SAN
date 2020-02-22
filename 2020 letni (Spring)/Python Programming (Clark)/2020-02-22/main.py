48a07bddea49426affdaf09da61017c9f1a4a9d4
kongra <kongra@gmail.com>
2020-02-22 10:35:03 +0100

    Update.

---
+++ 2020 letni (Spring)/Python Programming (Clark)/2020-02-22/main.py
@@ -0,0 +1,24 @@
+def distance(x, y):
+  return abs(x - y)
+
+# Ex 1. Write a procedure
+#       celsius2fahrenheit
+def celsius2fahrenheit(c):
+  return (c * 1.8) + 32
+
+print(celsius2fahrenheit(50))
+
+# Ex. 2. Write a procedure that
+# computes the area of a circle
+# with the given radius.
+
+# Ex. 3. Write a procedure that
+# computes the area of a rectangle
+
+# Ex. 4. Write a procedure that
+# computes the area of a triangle
+
+# Ex. 5. Write a procedure that
+# computes the area of a square
+# Hint: treat a square as a special
+# case of a rectangle
