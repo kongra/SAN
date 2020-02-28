bdadbc3ee7b2956e3555ae081a17a6545473b5dc
kongra <kongra@gmail.com>
2020-02-28 08:21:34 +0100

    Update.

---
+++ 2020 letni (Spring)/JiPP (dzienne)/src/san/jipp/Program1.java
@@ -0,0 +1,30 @@
+package san.jipp;
+
+import san.jipp.types.Age;
+
+public class Program1 {
+
+  static double foo(int n) {
+    return 4 * n + 3;
+  }
+
+  static double foo(double x) {
+    return 2 * x + 3;
+  }
+
+  public static void main(String... args) {
+    int n = 1;
+    foo(n);     //   1 : int
+    foo(0.5); // 0.5 : double
+
+    Object s = "abc";
+    Age age = Age.of(10).get();
+    System.out.println(s);
+    System.out.println(age);
+  }
+
+  // Zadanie 1. Napisz własną polimorficzną
+  // procedurę i zademonstruj jej działanie
+  // na obiektach kilku różnych typów.
+
+}
