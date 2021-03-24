c9fdf053de779ede3ad4e9179511839e6cb1ba0c
kongra <kongra@gmail.com>
2021-03-24 15:41:38 +0100

    New project for JEE lab created.

---
+++ 2021 letni/JEE1_dzienne_lab/src/san/jee1/TestServlet.java
@@ -0,0 +1,28 @@
+package san.jee1;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/TestServlet")
+public final class TestServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+       
+    public TestServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
