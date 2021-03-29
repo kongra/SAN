dcec631c63e5c525e2e7dbf3bb61b14231020cb3
kongra <kongra@gmail.com>
2021-03-29 16:32:03 +0200

    After lecture.

--- 2021 letni/FP (dzienne)/fp1/src/fp1/core.clj
+++ 2021 letni/FP (dzienne)/fp1/src/fp1/core.clj
@@ -24,18 +24,18 @@
 ;; FUNKCJE W CLOJURE
 
 ;; Definicja funkcji:
-(fn [<parameter-1> <parameter-2> ... <parameter-n>]
-  <body> := <s-expression-1> <s-expression-2> ... <s-expression-m>
-  )
+;; (fn [<parameter-1> <parameter-2> ... <parameter-n>]
+;;   <body> := <s-expression-1> <s-expression-2> ... <s-expression-m>
+;;   )
 
 ;; Wartość wywołania funkcji tak zdefiniowanej z pewnymi argumentami równa jest wartości
 ;; <s-expression-m> (inaczej, ostatniego wyrażenia)
 
-((fn [x y]
-   (println "Wywołano funkcję anonimową z argumentami" x y)
-   (+ x y))
- 1
- 2)
+#_((fn [x y]
+     (println "Wywołano funkcję anonimową z argumentami" x y)
+     (+ x y))
+   1
+   2)
 
 (def foo 4)
 
@@ -43,7 +43,7 @@
            (println "Działa goo z argumentami" x y)
            (+ x y 1)))
 
-(<f> <argument-1> <argument-2> ... <argument-n>)
+;; (<f> <argument-1> <argument-2> ... <argument-n>)
 
 (defn goo
   [x y]
@@ -81,3 +81,108 @@
 
 ;; Zadanie 2. Proszę napisać funkcję, która zwraca iloczyn liczb parzystych od 2 do 10000.
 ;; Zadanie 3. https://projecteuler.net/problem=2
+
+;; Problem: Wyznaczmy procedurę obliczającą pierwiastek kwadratowy z liczby x.
+;; Deklaratwnie: sqrt(x) dla x >= 0 jest to taka liczba y, że y^2 = x.
+;; Imperatywnie:
+;; - Zgadnij G
+;; - Ulepsz G ← (avg G (/ x G))
+;; - Nadal usprawniaj G aż będzie WYSTARCZAJĄCO DOBRE
+
+(defn abs [x]
+  (if (< x 0)
+    (- x)
+    x))
+
+(defn good-enough?
+  [G x]
+  (<= (abs (- x (square G))) 0.00000001))
+
+(defn avg [x y]
+  (/ (+ x y) 2))
+
+(defn improve
+  [G x]
+  (avg G (/ x G)))
+
+(defn heron-sqrt
+  [G x]
+  (println "heron-sqrt " G x)
+  (if (good-enough? G x)
+    ;; Nie ma potrzeby kontynuowania, G jest ok.
+    G
+
+    ;; W przeciwnym wypadku
+    (heron-sqrt (improve G x) x)))
+
+;; Zadanie 4. Proszę uzależnić przebieg procesu obliczania heron-sqrt od
+;;            parametru epsilon a nie od stałej (jak to ma miejsce obecnie).
+
+;; Zadanie 5. Proszę zrealizować procedurę obliczającą pierwiastek sześcienny z x.
+;;            Stosujemy wzorzec postępowania jak przy heron-sqrt. Hint: SICP.
+
+;; Model podstawieniowy na przykładzie:
+;; Aby wyznaczyć wartość KOMBINACJI (<op> <arg1> ... <arg N>)
+;; a. Poddaj ewaluacji operator <op> uzyskując procedurę
+;; b. Poddaj ewaluacji operandy <arg 1>, ..., <arg N> aby uzyskać wartości
+;;    aktualne parametrów formalnych (argumenty wywołania procedury)
+;; c. Zastosuj procedurę do argumentów
+;;    ca. Skopiuj ciało procedury podstawiając dostarczone argumenty w miejsce
+;;        odpowiednich parametrów fomalnych procedury
+;;    cb. Wyznacz wartość uzyskanego w ten sposób nowego ciała.
+
+(defn sum-of-squares
+  [x y]
+  (+ (square x) (square y)))
+
+;; (sum-of-squares 3 4)      =>
+;; (+ (square 3) (square 4)) =>
+;; (+ (*    3 3) (*    4 4)) =>
+;; (+ 9 16)                  =>
+;; 25
+
+;; Przykład: Arytmetyka Peano
+#_(defn peano+ [x y]
+    (println "peano+" x y)
+    (if (= x 0)
+      y
+      (peano+ (dec x) (inc y))))
+
+(defn peano+ [x y]
+  (println "peano+" x y)
+  (if (= x 0)
+    y
+    (recur (dec x) (inc y))))
+
+;; (peano+ 3 4)
+;; (peano+ (dec 3) (inc 4)) =>
+;; (peano+ 2 5)             =>
+;; (peano+ (dec 2) (inc 5)) =>
+;; (peano+ 1 6)             =>
+;; (peano+ (dec 1) (inc 6)) =>
+;; (peano+ 0 7)             =>
+;; 7
+
+;; W sensie czasu   (CPU) peano+ jest O[x].
+;; W sensie pamięci (RAM) peano+ jest O[1].
+
+;; TAKI PROCES OBLICZENIOWY JAK POWYŻEJ, KTÓRY MA STAŁĄ PAMIĘĆ
+;; (ROZMIAR PAMIĘCI JEST NIEZALEŻNY OD ROZMIARU PROBLEMU),
+;; NAZYWAMY ITERACJĄ.
+
+(defn peano+ [x y]
+  (if (= x 0)
+    y
+    (inc (peano+ (dec x) y))))
+
+;; (peano+ 3 4)                   =>
+;; (inc (peano+      2  4))       =>
+;; (inc (inc (peano+ 1  4)))      =>
+;; (inc (inc (inc (peano+ 0 4)))) =>
+;; (inc (inc (inc 4)))            =>
+;; (inc (inc 5))                  =>
+;; (inc 6)                        =>
+;; 7
+
+;; W sensie pamięci powyższa procedura jest O[x].
+;; TAKI PROCES NAZYWAMY REKURENCYJNYM.
