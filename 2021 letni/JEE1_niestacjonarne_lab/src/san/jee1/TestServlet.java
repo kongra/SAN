749cf6bac9bb51802440938385d9853a03501f54
kongra <kongra@gmail.com>
2021-04-10 09:44:24 +0200

    Update

---
+++ 2021 letni/JEE1_niestacjonarne_lab/src/san/jee1/TestServlet.java
@@ -0,0 +1,35 @@
+package san.jee1;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/TestServlet")
+public final class TestServlet extends HttpServlet {
+  
+  private static final long serialVersionUID = 1L;
+
+  public TestServlet() {
+    super();
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws ServletException, IOException {    
+    
+    var name = request.getParameter("name");
+    if (name == null) name = "";    
+    name = name.trim();
+    
+    response.setContentType("text/html; charset=UTF-8");
+    response.setCharacterEncoding("UTF-8");
+    
+    try (var writer = response.getWriter()) {
+      writer.append("<h1>Witaj ").append(name).append("</h1>");
+    }
+  }
+
+}
