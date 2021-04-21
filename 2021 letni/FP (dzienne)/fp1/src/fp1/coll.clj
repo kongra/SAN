76ecb9af6771cc33a2c9054117bc43d7b44fccea
kongra <kongra@gmail.com>
2021-04-21 18:23:04 +0200

    Update

--- 2021 letni/FP (dzienne)/fp1/src/fp1/coll.clj
+++ 2021 letni/FP (dzienne)/fp1/src/fp1/coll.clj
@@ -28,3 +28,51 @@
 ;; (iterate inc 0)
 ;; (0 (inc 0) (inc (inc 0)) (inc (inc (inc 0))) ... )
 ;; (0 1 2 3 4 ...)
+
+;; POJEMNIKI ASOCJACYJNE (MAPY)
+
+(def m1
+  {"0" "aaa"
+   "1" "bbb"
+   "2" 3.14
+   "3" [1 2 3 4]
+   "4" #{6 7}})
+
+;; fp1.core=> m1
+;; {"0" "aaa", "1" "bbb", "2" 3.14, "3" [1 2 3 4], "4" #{7 6}}
+;; fp1.core=> (get m1 "4")
+;; #{7 6}
+;; fp1.core=> (get m1 67)
+;; nil
+;; fp1.core=> (get m1 67 :nic-nie-ma)
+;; :nic-nie-ma
+;; fp1.core=> (m1 "4")
+;; #{7 6}
+;; fp1.core=> (m1 67)
+;; nil
+;; fp1.core=> (class m1)
+;; clojure.lang.PersistentArrayMap
+;; fp1.core=> (hash-map)
+;; {}
+;; fp1.core=> (class (hash-map))
+;; clojure.lang.PersistentArrayMap
+;; fp1.core=> (class (hash-map :a 1))
+;; clojure.lang.PersistentHashMap
+;; fp1.core=> (assoc m1 "6" 123)
+;; {"0" "aaa", "1" "bbb", "2" 3.14, "3" [1 2 3 4], "4" #{7 6}, "6" 123}
+;; fp1.core=> m1
+;; {"0" "aaa", "1" "bbb", "2" 3.14, "3" [1 2 3 4], "4" #{7 6}}
+;; fp1.core=> (assoc m1 "6" 123 "7" 432)
+;; {"0" "aaa", "1" "bbb", "2" 3.14, "3" [1 2 3 4], "4" #{7 6}, "6" 123, "7" 432}
+;; fp1.core=> m1
+;; {"0" "aaa", "1" "bbb", "2" 3.14, "3" [1 2 3 4], "4" #{7 6}}
+;; fp1.core=> (dissoc m1 "0")
+;; {"1" "bbb", "2" 3.14, "3" [1 2 3 4], "4" #{7 6}}
+;; fp1.core=> m1
+;; {"0" "aaa", "1" "bbb", "2" 3.14, "3" [1 2 3 4], "4" #{7 6}}
+;; fp1.core=> (class m1)
+;; clojure.lang.PersistentArrayMap
+;; fp1.core=> (instance? java.util.Map m1)
+;; true
+;; fp1.core=> (.get m1 "0")
+;; "aaa"
