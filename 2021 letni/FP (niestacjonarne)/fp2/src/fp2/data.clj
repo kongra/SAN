42959b6ac6a59c5f181c38ef3140460a27e9c975
kongra <kongra@gmail.com>
2021-05-15 14:11:47 +0200

    Update

--- 2021 letni/FP (niestacjonarne)/fp2/src/fp2/data.clj
+++ 2021 letni/FP (niestacjonarne)/fp2/src/fp2/data.clj
@@ -70,4 +70,104 @@
 ;; (find-index (list) "a") => nil
 
 ;; 2. ZBIORY
+;; * Zbiór nie zawiera powtórzeń elementów.
+;; * Uzyskanie informacji o przynależności obiektu do zbioru realizowane jest ze
+;;   złożonością O(1) lub O(log(n)).
+;; * Zbiór nie utrzymuje porządku (kolejności) elementów(**)
+
+;; ** - są zbiory utrzymujące porządek, np. tree-set
+
+;; fp2.core=> (hash-set "a" "b" "c" "d")
+;; #{"d" "a" "b" "c"}
+;; fp2.core=> (def s1 (hash-set "a" "b" "c" "d" "a" "d" "c"))
+;; #'fp2.core/s1
+;; fp2.core=> s1
+;; #{"d" "a" "b" "c"}
+;; fp2.core=> (contains? s1 "a")
+;; true
+;; fp2.core=> (contains? s1 "x")
+;; false
+;; fp2.core=> (s1 "a")
+;; "a"
+;; fp2.core=> (s1 "x")
+;; nil
+;; fp2.core=>
+;; fp2.core=> (class s1)
+;; clojure.lang.PersistentHashSet
+;; fp2.core=> (def s2 (conj s1 "x"))
+;; #'fp2.core/s2
+;; fp2.core=> s2
+;; #{"d" "x" "a" "b" "c"}
+;; fp2.core=> s1
+;; #{"d" "a" "b" "c"}
+;; fp2.core=> (def s2 (disj s1 "a"))
+;; #'fp2.core/s2
+;; fp2.core=> s2
+;; #{"d" "b" "c"}
+;; fp2.core=> (clojure.set/difference s2 s1)
+;; #{}
+;; fp2.core=> (clojure.set/difference s1 s2)
+;; #{"a"}
+;; fp2.core=> (clojure.set/intersection s1 s2)
+;; #{"d" "b" "c"}
+;; fp2.core=> (clojure.set/union s1 s2)
+;; #{"d" "a" "b" "c"}
+;; fp2.core=> (clojure.set/subset? s1 s2)
+;; false
+;; fp2.core=> (clojure.set/subset? s2 s1)
+;; true
+
+;; Zadanie 5. proszę zrealizować procecurę unique-seq, która ma następujące działanie:
+;; (unique-seq [1 2 3 4 1 1 2 2 7 7 3 2 1]) => (1 2 3 4 7)
+;; Innymi słowy, procedura ta utrzymuje kolejność elementów w oryginalnej sekwencji,
+;; ale eliminuje duplikaty.
+;; Hint: wykorzystać hash-set.
+
 ;; 3. MAPY (POJEMNIKI ASOCJACYJNE)
+;; Są to kolekcje, w których występuje skojarzenie kluczy oraz wartości.
+;; String[] tab = {"a", "b", "c"};
+;; 0 -> "a"
+;; 1 -> "b"
+;; 2 -> "c"
+
+;; "a" -> 0
+;; "b" -> 1
+;; "c" -> 2
+;; fp2.core=> (hash-map "a" 0, "b" 1, "c" 2)
+;; {"a" 0, "b" 1, "c" 2}
+;; fp2.core=> (def m1 (hash-map "a" 0, "b" 1, "c" 2))
+;; #'fp2.core/m1
+;; fp2.core=> (get m1 "a")
+;; 0
+;; fp2.core=> (get m1 "b")
+;; 1
+;; fp2.core=> (get m1 "c")
+;; 2
+;; fp2.core=> (get m1 "x")
+;; nil
+;; fp2.core=> (get m1 "x" "wartość domyślna")
+;; "wartość domyślna"
+;; fp2.core=> (m1 "a")
+;; 0
+;; fp2.core=> (m1 "b")
+;; 1
+;; fp2.core=> (m1 "c")
+;; 2
+;; fp2.core=> (m1 "x")
+;; nil
+;; fp2.core=> (class m1)
+;; clojure.lang.PersistentHashMap
+;; fp2.core=> (def m2 (assoc m1 "x" 10))
+;; #'fp2.core/m2
+;; fp2.core=> m2
+;; {"x" 10, "a" 0, "b" 1, "c" 2}
+;; fp2.core=> m1
+;; {"a" 0, "b" 1, "c" 2}
+;; fp2.core=> (def m3 (dissoc m1 "a"))
+;; #'fp2.core/m3
+;; fp2.core=> m3
+;; {"b" 1, "c" 2}
+;; fp2.core=> (merge {"a" 1 "b" 2} {"c" 3})
+;; {"a" 1, "b" 2, "c" 3}
+;; fp2.core=> (merge {"a" 1 "b" 2} {"c" 3 "b" 10})
+;; {"a" 1, "b" 10, "c" 3}
