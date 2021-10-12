fb9991e62cdca10edc747ecce44527cac3fa2866
kongra <kongra@gmail.com>
2021-10-12 16:26:53 +0200

    Update

---
+++ 2021 zimowy/PP (wykład dzienne)/2021-10-12/Program.java
@@ -0,0 +1,9 @@
+class Program {
+  public static void main(String[] args) {
+    int n = 256;
+    int m = n * 10;
+
+    System.out.println("Cześć");
+    System.out.println("m ma wartość " + m);
+  }
+}
