829821ae8430511e705bebd3e019bf77ae9a6224
kongra <kongra@gmail.com>
2022-03-19 20:09:09 +0100

    Update

--- 2022 letni/Systemy-szkieletowe-bdia-niestacjonarne/JEE-niestacjonarne/sh/sonarqube.sh
+++ 2022 letni/Systemy-szkieletowe-bdia-niestacjonarne/JEE-niestacjonarne/sh/sonarqube.sh
@@ -2,4 +2,4 @@
 mvn clean verify sonar:sonar \
   -Dsonar.projectKey=JEE-niestacjonarne-1 \
   -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=cf3ee64e75470373ff40406f2077dd4d52513824
+  -Dsonar.login=4b24fd3a4274ddf58b0bc538b9e80aa0ae74807b
