b2e73b9fa0ff434a7693892c9be47ab9afdb87b6
kongra <kongra@gmail.com>
2022-03-20 08:41:26 +0100

    Update

--- 2022 letni/FP-niestacjonarne/2022-03-05/fp1/src/fp1/core.clj
+++ 2022 letni/FP-niestacjonarne/2022-03-05/fp1/src/fp1/core.clj
@@ -1,6 +1,32 @@
 (ns fp1.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn foo [x]
+  (+ x 3))
+
+;; Przykład. Zdefiniuj procedurę silnia/factorial
+;; Dla danego n == 0 zwróć 1
+;; Dla danego n >  0 zwróć n * silnia (n-1)
+
+(defn factorial [n]
+  (if (= n 0)
+    1
+
+    (*' n (factorial (- n 1)))))
+
+;; Wyrażenie warunkowe if
+#_(if <predicate>
+    <consequent>
+    <alternative>)
+
+;; Jeżeli <predicate> (predykat) jest spełniony, tj. ma wartość prawdy logicznej, to
+;; wartością całego wyrażenia warunkowego jest wartość wyrażenia <consequent> (następnika)
+;; w przeciwnym wypadku wartością całego wyrażenia warunkowego jest wartość
+;; <alternative> (alternatywy).
+
+;; <predicate> ? <consequent> : <alternative> - tzw. ternary conditional
+;; np. int n = i == 10 ? 3 : 4;
+
+;; Zadanie 1. Zrealizuj w Clojure procedurę obliczającą n-ty wyraz ciągu Fibonacciego
+;; fib(0) => 0
+;; fib(1) => 1
+;; fib(n | n > 1) => fib(n-1) + fib(n-2)
