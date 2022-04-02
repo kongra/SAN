c9c49e128acddb589e1f5e6030f1d1afcf2bd066
kongra <kongra@gmail.com>
2022-04-02 12:48:00 +0200

    Update

--- 2022 letni/FP-niestacjonarne/2022-03-05/fp1/src/fp1/core.clj
+++ 2022 letni/FP-niestacjonarne/2022-03-05/fp1/src/fp1/core.clj
@@ -30,3 +30,67 @@
 ;; fib(0) => 0
 ;; fib(1) => 1
 ;; fib(n | n > 1) => fib(n-1) + fib(n-2)
+
+(defn abs- [x]
+  (if (< x 0)
+    (- x)
+
+    x))
+
+(defn square [x] (* x x))
+
+(defn good-enough? [G x]
+  (< (abs- (- x (square G))) 0.00001))
+
+(defn avg [x y]
+  (/ (+ x y) 2))
+
+(defn improve [G x]
+  #_(println "improve...")
+  (avg G (/ x G)))
+
+(defn sqrt [G x]
+  (if (good-enough? G x)
+    G
+    (sqrt (improve G x) x)))
+
+;; Zadanie 2.
+;; a. Przebieg procedury Herona uzależnić od parametru
+;;    formalnego Epsilon
+;; b. Przebieg procedury Herona uzależnić od parametru
+;;    formalnego N (ilości kroków)
+
+(defn sum-of-squares [x y]
+  (+ (square x) (square y)))
+
+(sum-of-squares 3 4)
+(+ (square 3) (square 4))
+(+ (* 3 3) (* 4 4))
+(+ 9 16)
+25
+
+(+ 1 2 3 4 5)
+
+(defn if- [pred consequent alternative])
+
+(def x 0)
+(if- (zero? x) :nothing (/ 1 x))
+
+;; ARYTMETYKA PEANO
+(defn p+ [x y]
+  (if (= x 0)
+    y
+    (p+ (dec x) (inc y))))
+
+(p+ 3 4)
+(if (= 3 0) 4 (p+ (dec 3) (inc 4)))
+(p+ (dec 3) (inc 4))
+(p+ 2 5)
+(if (= 2 0) 5 (p+ (dec 2) (inc 5)))
+(p+ (dec 2) (inc 5))
+(p+ 1 6)
+(if (= 1 0) 6 (p+ (dec 1) (inc 6)))
+(p+ (dec 1) (inc 6))
+(p+ 0 7)
+(if (= 0 0) 7 (p+ (dec 0) (inc 7)))
+7
