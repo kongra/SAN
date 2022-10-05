f4a5231bece11c4c968eb4f57e526834809c82ce
kongra <kongra@gmail.com>
2022-10-05 16:54:46 +0200

    New project started

---
+++ 2022 zimowy/Technologie programowania - stacjonarne 7 sem./techprog1/src/main/java/edu/san/MyRemoteService.java
@@ -0,0 +1,37 @@
+package edu.san;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * To use it via injection.
+ *
+ * {@code
+ *     @Inject
+ *     @RestClient
+ *     MyRemoteService myRemoteService;
+ *
+ *     public void doSomething() {
+ *         Set<MyRemoteService.Extension> restClientExtensions = myRemoteService.getExtensionsById("io.quarkus:quarkus-rest-client");
+ *     }
+ * }
+ */
+@RegisterRestClient(baseUri = "https://stage.code.quarkus.io/api")
+public interface MyRemoteService {
+
+    @GET
+    @Path("/extensions")
+    Set<Extension> getExtensionsById(@QueryParam("id") String id);
+
+    class Extension {
+        public String id;
+        public String name;
+        public String shortName;
+        public List<String> keywords;
+    }
+}
