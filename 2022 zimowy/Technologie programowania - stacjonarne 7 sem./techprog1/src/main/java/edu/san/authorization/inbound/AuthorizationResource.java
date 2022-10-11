11d364fda81e23b5ac8557cab5072a9ebc58dd13
kongra <kongra@gmail.com>
2022-10-11 20:35:26 +0200

    Refactoring, renaming.

--- 2022 zimowy/Technologie programowania - stacjonarne 7 sem./techprog1/src/main/java/edu/san/authorization/inbound/AuthorizationResource.java
+++ 2022 zimowy/Technologie programowania - stacjonarne 7 sem./techprog1/src/main/java/edu/san/authorization/inbound/AuthorizationResource.java
@@ -1,5 +1,6 @@
 package edu.san.authorization.inbound;
 
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -8,15 +9,16 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/authorization")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 class AuthorizationResource {
 
   @POST
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
-  Response createProfile(ProfileDTO profileDTO) {
+  @Path("/sign-up")
+  Response signUp(@Valid SignUpData signUpData) {
     // TODO: Use JSON-P for Response
-    
+
     return null;
   }
-  
+
 }
---
+++ 2022 zimowy/Technologie programowania - stacjonarne 7 sem./techprog1/src/main/java/edu/san/authorization/inbound/SignUpData.java
@@ -0,0 +1,16 @@
+package edu.san.authorization.inbound;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+class SignUpData {
+
+  String email;
+
+  String firstName;
+
+  String lastName;
+
+}
