e6246a0c80b8aaea2e9e9c88a91d2714c2fdf9dd
kongra <kongra@gmail.com>
2022-10-14 21:02:21 +0200

    A simple integration test added.

---
+++ 2022 zimowy/Technologie programowania - stacjonarne 7 sem./techprog1/src/test/java/edu/san/authentication/inbound/AuthenticationResourceTest.java
@@ -0,0 +1,42 @@
+package edu.san.authentication.inbound;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+
+import javax.json.Json;
+
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+class AuthenticationResourceTest {
+
+  private AuthenticationResourceClient authenticationResourceClient;
+  
+  @BeforeEach
+  void setUp() {
+    final var uri = URI.create("http://localhost:8080");
+    authenticationResourceClient = RestClientBuilder
+        .newBuilder()
+        .baseUri(uri)
+        .build(AuthenticationResourceClient.class);
+  }
+  
+  @Test
+  void testSignUp() {
+    final var signUpData = Json.createObjectBuilder()
+        .add("email", "kongra@gmail.com")
+        .add("firstName", "Konrad")
+        .add("lastName", "Grzanek")
+        .build();
+    
+    try (var response = authenticationResourceClient.signUp(signUpData)) {
+      assertThat(response.getStatus()).isEqualTo(200);
+    }
+  }
+
+}
