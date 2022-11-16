91a3c5128c050b7577509d69a8108917b1f959c2
kongra <kongra@gmail.com>
2022-11-16 10:40:50 +0100

    Update.

--- 2022 zimowy/Technologie programowania - niestacjonarne 7 sem./techprog2/src/main/java/edu/san/indexer/control/IndexerFacade.java
+++ 2022 zimowy/Technologie programowania - niestacjonarne 7 sem./techprog2/src/main/java/edu/san/indexer/control/IndexerFacade.java
@@ -1,7 +1,5 @@
 package edu.san.indexer.control;
 
-import java.util.Objects;
-
 import javax.enterprise.context.ApplicationScoped;
 
 import telsos.architecture.hexagonal.annotations.Port;
@@ -11,10 +9,6 @@ import telsos.architecture.hexagonal.annotations.PortType;
 @ApplicationScoped
 public class IndexerFacade {
 
-  private final DataReader dataReader;
-
-  public IndexerFacade(DataReader dataReader) {
-    this.dataReader = Objects.requireNonNull(dataReader);
-  }
+  // To be filled later
 
 }
--- 2022 zimowy/Technologie programowania - niestacjonarne 7 sem./techprog2/src/test/java/edu/san/indexer/entity/DataReaderImplTest.java
+++ 2022 zimowy/Technologie programowania - niestacjonarne 7 sem./techprog2/src/test/java/edu/san/indexer/entity/DataReaderImplTest.java
@@ -21,15 +21,15 @@ class DataReaderImplTest {
 
   @Test
   void testReadFirstNames() throws IOException {
-    var file = Paths.get(
+    final var file = Paths.get(
         "/media/kongra/Devel/Projects/Present/SAN/",
         "2022 zimowy/Warstwy integracji i Big Data - 7 sem./",
         "IMIONA_NADANE_DZIECIOM_W_POLSCE_W_I_POŁOWIE_2022_R._-_IMIĘ_PIERWSZE.csv");
-    
-    Stopwatch timer = Stopwatch.start();    
-    var firstNames = dataReader.readFirstNames(file);
+
+    final var timer = Stopwatch.start();
+    final var firstNames = dataReader.readFirstNames(file);
     assertThat(firstNames).isNotEmpty();
-    
+
     System.out.println(timer);
     System.out.println(firstNames.size());
   }
