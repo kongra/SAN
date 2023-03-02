9e8b0a6ed4da4219fd853690b7b17f0bdb53e6db
Konrad Grzanek <kongra@gmail.com>
2023-03-02 10:32:45 +0100

    Cleanups

--- 2022 letni/Serwery-aplikacyjne-niestacjonarne/liberty-test-one/src/main/java/edu/san/rest/RestApplication.java
+++
@@ -1,7 +0,0 @@
-package edu.san.rest;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class RestApplication extends Application {}
