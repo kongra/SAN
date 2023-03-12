160b2c230329b9a8f247aaad9594e6820f14aa87
Konrad Grzanek <kongra@gmail.com>
2023-03-12 08:33:42 +0100

    vanilla techprog-3 added

--- 2023 letni/BDiA/techprog3/scripts/sql/dev (done)/2022-10-11 16_14_00 CEST add column firstName to table profile.sql
+++
@@ -1,9 +0,0 @@
-begin;
-
-alter table profile
-drop column if exists lastName;
-
-alter table profile
-add column lastName text not null;
-
-commit;
--- 2023 letni/BDiA/techprog3/scripts/sql/dev (done)/2022-10-18 15_39_00 CEST make profles email unique.sql
+++
@@ -1,6 +0,0 @@
-begin;
-
-drop index if exists profile_email_index;
-create unique index profile_email_index on profile(email);
-
-commit;
