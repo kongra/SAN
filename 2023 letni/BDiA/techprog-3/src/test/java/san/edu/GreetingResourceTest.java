847e8a9953b00d51266d15bce374672d597a570c
Konrad Grzanek <kongra@gmail.com>
2023-03-12 08:54:42 +0100

    Update to match Java 19 and other stuff.

--- 2023 letni/BDiA/techprog-3/src/test/java/san/edu/GreetingResourceTest.java
+++ 2023 letni/BDiA/techprog-3/src/test/java/san/edu/GreetingResourceTest.java
@@ -1,21 +1,23 @@
 package san.edu;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-@QuarkusTest
-public class GreetingResourceTest {
+import org.junit.jupiter.api.Test;
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
-    }
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+@SuppressWarnings("static-method")
+class GreetingResourceTest {
+
+  @Test
+  void testHelloEndpoint() {
+    given()
+        .when().get("/hello")
+        .then()
+        .statusCode(200)
+        .body(is("Hello RESTEasy"));
+  }
 
 }
