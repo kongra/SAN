6aeee09425a6793971b144efd278a345d9503948
Konrad Grzanek <kongra@gmail.com>
2023-10-08 18:29:49 +0200

    Update

---
+++ 2023 zimowy/Zjazdy B/Wzorce/techprog-4/src/test/edu/san/patterns/singleton/SingletonTest.java
@@ -0,0 +1,20 @@
+// © 2023 Konrad Grzanek <kongra@gmail.com>
+package edu.san.patterns.singleton;
+
+// import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class SingletonTest {
+
+  @Test
+  void test() {
+    Singleton.foo();
+    SingletonOnDemand.goo();
+    
+    // final var singleton = Singleton.getInstance();
+    // final var singleton2 = Singleton.getInstance();
+    // assertThat(singleton).isEqualTo(singleton2);
+  }
+
+}
