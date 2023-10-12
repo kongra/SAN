1dcbab065b363541266a3a18f9568aad1c9e3d85
Konrad Grzanek <kongra@gmail.com>
2023-10-12 16:13:47 +0200

    Update

--- 2023 zimowy/Dzienne/Wzorce/techprog-5/src/main/java/edu/san/Singleton.java
+++ 2023 zimowy/Dzienne/Wzorce/techprog-5/src/main/java/edu/san/Singleton.java
@@ -1,8 +1,18 @@
 package edu.san;
 
-public class Singleton {
+public final class Singleton {
+
+  private static final Singleton INSTANCE = new Singleton();
+
+  public static Singleton getInstance() {
+    return INSTANCE;
+  }
 
   public static void foo() {
     System.out.println("Singleton::foo()");
+  }
+
+  private Singleton() {
+    System.out.println("Singleton::new()");
   }
 }
--- 2023 zimowy/Dzienne/Wzorce/techprog-5/src/test/java/edu/san/SingletonTest.java
+++ 2023 zimowy/Dzienne/Wzorce/techprog-5/src/test/java/edu/san/SingletonTest.java
@@ -2,11 +2,17 @@ package edu.san;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class SingletonTest {
 
   @Test
   void test1() {
     Singleton.foo();
+
+    var instance1 = Singleton.getInstance();
+    var instance2 = Singleton.getInstance();
+    assertThat(instance1).isSameAs(instance2);
   }
 
 }
