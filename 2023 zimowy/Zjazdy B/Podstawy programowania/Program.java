75a52cf1ca1621137eb460790ff3e5fad8b4d0b5
Konrad Grzanek <kongra@gmail.com>
2023-11-05 16:50:18 +0100

    Update

---
+++ 2023 zimowy/Zjazdy B/Podstawy programowania/Program.java
@@ -0,0 +1,11 @@
+class Program {
+
+  public static void main(String[] args) {
+    int i = 10;
+    i = i + 3;
+    i = i + "abcd";
+
+    System.out.println("Witaj Świecie!!!");
+  }
+
+}
---
+++ 2023 zimowy/Zjazdy B/Podstawy programowania/program.py
@@ -0,0 +1,7 @@
+i = 10
+i = i + 3
+
+i = i + "abcd"
+
+print(i)
+print("Witaj Świecie!!!")
