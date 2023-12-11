03f1fcacc41474b7f88943172c1bc80e5640a422
Konrad Grzanek <kongra@gmail.com>
2023-12-11 09:04:20 +0100

    Update

---
+++ 2023 zimowy/Zjazdy B/techprog-4/src/main/java/edu/san/app/inventory/employee/EmployeeEntityRepositoryImpl.java
@@ -0,0 +1,9 @@
+// © 2023 Konrad Grzanek <kongra@gmail.com>
+package edu.san.app.inventory.employee;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+class EmployeeEntityRepositoryImpl implements EmployeeEntityRepository {
+
+}
