e307a73de0b61454420f3821d6019be524d7e914
Konrad Grzanek <kongra@gmail.com>
2024-03-14 15:46:51 +0100

    Update

--- 2024 letni/Dzienne/PiKSA/piksa-1/src/test/java/edu/san/GreetingResourceTest.java
+++ 2024 letni/Dzienne/PiKSA/piksa-1/src/test/java/edu/san/GreetingResourceTest.java
@@ -1,14 +1,16 @@
 package edu.san;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
 @QuarkusTest
+@SuppressWarnings("static-method")
 class GreetingResourceTest {
-  
+
   @Test
   void testHelloEndpoint() {
     given()
