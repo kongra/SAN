20e298a66fe670bfb78fc164df75012405091d72
Konrad Grzanek <kongra@gmail.com>
2024-03-26 16:35:07 +0100

    Update

---
+++ 2024 letni/Dzienne/JiPP/jipp/src/main/java/edu/san/Program.java
@@ -0,0 +1,10 @@
+// © 2024 Konrad Grzanek <kongra@gmail.com>
+package edu.san;
+
+public class Program {
+
+  public static void main(String[] args) {
+    System.out.println("Witaj Świecie!!!");
+  }
+
+}
