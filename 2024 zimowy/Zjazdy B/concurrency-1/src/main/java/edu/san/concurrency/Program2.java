d7e38b3e3608dcb7053f2c0b744ff2164e28e7cc
Konrad Grzanek <kongra@gmail.com>
2024-10-14 20:32:18 +0200

    Update

---
+++ 2024 zimowy/Zjazdy B/concurrency-1/src/main/java/edu/san/concurrency/Program2.java
@@ -0,0 +1,12 @@
+// © 2024 Konrad Grzanek <kongra@gmail.com>
+package edu.san.concurrency;
+
+class Program2 {
+
+  public static void main(String[] args) {
+    int i = 1;
+    int j = i + 4;
+    System.out.println(j);
+  }
+
+}
--- 2024 zimowy/Zjazdy B/patterns-1/src/main/java/edu/san/patterns/Singleton.java
+++ 2024 zimowy/Zjazdy B/patterns-1/src/main/java/edu/san/patterns/Singleton.java
@@ -7,12 +7,30 @@ public class Singleton {
     private static final Singleton INSTANCE = new Singleton();
   }
 
+//   private static Singleton INSTANCE;
+
+//  public static Singleton getInstance() {
+//    if (INSTANCE == null) {
+//      INSTANCE = new Singleton();
+//    }
+//    return INSTANCE;
+//  }
+
   public static Singleton getInstance() {
     return SingletonHolder.INSTANCE;
   }
 
+  public static void test1() {
+    System.out.println("Działa test1()");
+  }
+
   private Singleton() {
     System.out.println("Działa Singleton()");
+  }
+
+  @SuppressWarnings("static-method")
+  public void draw() {
+    System.out.println("Działa Singleton.draw()");
   }
 
 }
